99a89ee8a08ff3a0221873d95b249ecba947aea4
Giorgio Giannotta <jorge_upa@msn.com>
2023-08-29 07:08:26 +0200

    Refactor CardDetails

--- lib/card_details.dart
+++ lib/card_details.dart
@@ -20,10 +20,16 @@ class _CardDetailsState extends State<CardDetails> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text("Image"),
-            Text("Title"),
-            Text("Information"),
-            Text("Booking Button"),
+            Image.network(widget.selectedLocation.imageUrl),
+            Text(
+              widget.selectedLocation.title,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+            ElevatedButton(onPressed: () {}, child: Text("Booking Now")),
           ],
         ),
       ),
